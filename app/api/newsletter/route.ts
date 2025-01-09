f4fbeb0a2182812f8935ae77de12fd0f21eefc95
RE <qq1609403959@outlook.com>
2025-01-09 23:17:35 +0800

    fix build

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -5,7 +5,7 @@ export const dynamic = 'force-static'
 
 const handler = NewsletterAPI({
   // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
+  provider: siteMetadata.newsletter?.provider,
 })
 
 export { handler as GET, handler as POST }
