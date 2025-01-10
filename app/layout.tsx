caa798dbe3e0cb1f964a56095c8ffc7b65625d2d
re-ovo <qq1609403959@outlook.com>
2025-01-10 11:06:09 +0800

    优化UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,13 +97,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
-          <SectionContainer>
-            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-              <Header />
+          <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+            <Header />
+            <SectionContainer>
               <main className="mb-auto">{children}</main>
-            </SearchProvider>
-            <Footer />
-          </SectionContainer>
+            </SectionContainer>
+          </SearchProvider>
+          <Footer />
         </ThemeProviders>
       </body>
     </html>
